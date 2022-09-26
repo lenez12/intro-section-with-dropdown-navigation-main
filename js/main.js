78170716d5f7947f8613cf82a73bd7bf2d014c0e
jery lenas <jerylenas@gmail.com>
2022-09-26 13:50:16 +0700

    1. create responsive navigation to desktop navigation 2. create home section

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ function closeNav() {
 		e.classList.remove("active");
 	});
 }
+
+// let dropdownMenu = document.getElementById("dropdown-menu");
+// let dropdownToggle = document.getElementById("dropdown-toggle");
+// function openSubmenu() {
+// 	dropdownMenu.classList.toggle("active");
+// 	dropdownToggle.classList.toggle("active");
+// }
+
+// interaction with dropdown
+const navBar = document.getElementById("navbar-nav");
+const navDropdown = navBar.querySelectorAll(".nav_item.dropdown");
+navDropdown.forEach((element) => {
+	let link = element.getElementsByClassName("nav_link")[0];
+	let dropdown = element.getElementsByClassName("dropdown_menu")[0];
+	let arrow = link.getElementsByClassName("dropdown_toggle")[0];
+
+	link.addEventListener("click", () => {
+		// Check all dropdown if it open an close it
+		// navDropdown.forEach((elements, index) => {
+		// 	let links = elements.getElementsByClassName("nav_link")[0];
+		// 	let arrows = links.getElementsByClassName("dropdown_toggle")[0];
+		// 	let dropdowns = elements.getElementsByClassName("dropdown_menu")[0];
+		// 	if (dropdowns.classList.contains("active")) {
+		// 		console.log("element Dropdown " + index + " contain");
+		// 		dropdowns.classList.remove("active");
+		// 		arrows.classList.remove("active");
+		// 	}
+		// });
+
+		//open selected dropdown menu and
+		dropdown.classList.toggle("active");
+		arrow.classList.toggle("active");
+	});
+});
